e922716e2804a7704458a460d234bd30673aa0d9
aviau <alexandre.viau@savoirfairelinux.com>
2014-12-23 10:51:58 -0500

    Default client version to 1_0
    Change-Id: I2b7c2e421c21cab7fc640c192963160309ee3d37

--- surveilclient/client.py
+++ surveilclient/client.py
@@ -16,7 +16,7 @@
 from surveilclient.common import utils
 
 
-def Client(version, *args, **kwargs):
+def Client(version='1_0', *args, **kwargs):
     module = utils.import_versioned_module(version, 'client')
     client_class = getattr(module, 'Client')
     return client_class(*args, **kwargs)

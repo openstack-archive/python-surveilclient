eebb4ad6c8c6b46113f56920460cfde4fa7035cc
aviau <alexandre.viau@savoirfairelinux.com>
2015-04-21 17:54:50 -0400

    Implemented V2_0 API
    Change-Id: I0b5b42abe75ff9f6d0b2cbe87a240c0766e3310e

---
+++ surveilclient/v2_0/status/__init__.py
@@ -0,0 +1,26 @@
+# Copyright 2015 - Savoir-Faire Linux inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+from surveilclient.common import surveil_manager
+from surveilclient.v2_0.status import hosts
+from surveilclient.v2_0.status import services
+
+
+class StatusManager(surveil_manager.SurveilManager):
+    base_url = '/status'
+
+    def __init__(self, http_client):
+        super(StatusManager, self).__init__(http_client)
+        self.hosts = hosts.HostsManager(self.http_client)
+        self.services = services.ServicesManager(self.http_client)

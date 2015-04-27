fbbb5edb87f82f57125a53703696b8bd777f7c74
aviau <alexandre.viau@savoirfairelinux.com>
2015-04-27 13:45:51 -0400

    Implemented downtime API
    Change-Id: I4ef5f2c671be20723a3ad4a2aba2623dfd7711d9

--- surveilclient/v2_0/actions/acknowledge.py
+++ surveilclient/v2_0/actions/acknowledge.py
@@ -13,14 +13,15 @@
 # under the License.
 
 from surveilclient.common import surveil_manager
-from surveilclient.v2_0.actions import acknowledge
-from surveilclient.v2_0.actions import downtime
 
 
-class ActionsManager(surveil_manager.SurveilManager):
-    base_url = '/actions'
+class AcknowledgeManager(surveil_manager.SurveilManager):
+    base_url = '/actions/acknowledge'
 
-    def __init__(self, http_client):
-        super(ActionsManager, self).__init__(http_client)
-        self.downtime = downtime.DowntimeManager(self.http_client)
-        self.acknowledge = acknowledge.AcknowledgeManager(self.http_client)
+    def create(self, **kwargs):
+        resp, body = self.http_client.json_request(
+            self.base_url,
+            'POST',
+            body=kwargs,
+        )
+        return body
